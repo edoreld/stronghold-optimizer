c7bb54979abe180342577925226155155b062ee0
Clarence Olivier <edoreld@gmail.com>
2026-03-23 11:42:39 +0100

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default { base: '/stronghold-optimizer/' }
-
-
+export default defineConfig({
+  base: '/stronghold-optimizer/',
+  plugins: [react()],
+})
